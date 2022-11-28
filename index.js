723825f143a34c8f0f91ce1b118a6e8184ad5b02
Braintech22 <datveryjimmy@gmail.com>
2022-11-27 20:06:49 -0500

    m

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function login() {
     } else {
         alert('Thank You for Login & You are Redirecting to smart factory');
         //Redirecting to other page or webste code or you can set your own html page.
-        window.location = "index.html";
+        window.location = "main.html";
     }
 }
 //Reset Inputfield code.
